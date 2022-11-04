773b0275f4d717bf7efa7cfb9e74ca157d9b2b71
IProroIK <grraff1230@gmail.com>
2022-11-04 18:06:01 +0200

    Delete GetAngle Method

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -17,12 +17,7 @@ public class Door : MonoBehaviour
     [SerializeField] private ParticleSystem _warpEffect;
 
     private bool _isFeet = true;
-    public float GetAngileToGetThrough()
-    {
-        if (_angileToGetThrough > 360 || _angileToGetThrough < 0)
-            throw new Exception("Not Possible angile, please set from 0 to 360");
-        return _angileToGetThrough;
-    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent(out Player player))
